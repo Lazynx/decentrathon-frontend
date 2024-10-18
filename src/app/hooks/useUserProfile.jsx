0927671415167bc2631f4af01c405f573c3f494e
Lazynx <frichaplay@gmail.com>
2024-10-19 03:18:22 +0500

    [fix]: hotfix

--- src/app/hooks/useUserProfile.jsx
+++ src/app/hooks/useUserProfile.jsx
@@ -14,10 +14,11 @@ export const useUserProfile = () => {
   const fetchProfile = async () => {
     try {
       const telegramId = localStorage.getItem('telegramId'); 
+      const response = await axios.put(`${process.env.NEXT_PUBLIC_ORIGIN}/auth/userInfo`, { telegramId });
     //   const response = await axiosInstance.put('/auth/userInfo', { telegramId });
-      const response = await axios.create({
-        baseURL: `${process.env.NEXT_PUBLIC_ORIGIN}/auth/userInfo`,
-      }, { telegramId });
+    //   const response = await axios.create({
+    //     baseURL: `${process.env.NEXT_PUBLIC_ORIGIN}/auth/userInfo`,
+    //   }, { telegramId });
       const user = response.data.data.user; 
 
       setUserData({
